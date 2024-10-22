28284b0e1002137db7a02922db704da5b96649ce
VanceGoblin <zvkubajak@gmail.com>
2024-10-22 10:45:24 -0500

    Updated Weather to Weather[] for Forecast Array & Created WeatherItem Interface

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,6 +1,22 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Interface for item in fetchWeatherData method.
+interface WeatherItem {
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: Array<{
+    icon: string;
+    description: string;
+  }>;
+  wind: {
+    speed: number;
+  };
+  dt_txt: string;
+}
+
 // TODO: Define an interface for the Coordinates object
 
 interface Coordinates {
@@ -72,7 +88,7 @@ class WeatherService {
   }
 
   // TODO: Create fetchWeatherData method
-  private async fetchWeatherData(coordinates: Coordinates, city: string): Promise<Weather> {
+  private async fetchWeatherData(coordinates: Coordinates, city: string): Promise<Weather[]> {
     const weatherURL = `${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.APIkey}`;
 
     try {
@@ -88,7 +104,7 @@ class WeatherService {
         throw new Error(`No weather data found with coordinates: (${coordinates.lat}, ${coordinates.lon}).`)
       }
 
-      const weatherData: Weather = data.list.map((item: any) => {
+      const weatherData: Weather[] = data.list.map((item: WeatherItem) => {
         const tempK = item.main.temp; // Temp is in Kelvin from API
         const tempF = (tempK - 273.15) * 9/5 + 32;
 
@@ -116,7 +132,7 @@ class WeatherService {
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  async getWeatherForCity(city: string): Promise<Weather> {
+  async getWeatherForCity(city: string): Promise<Weather[]> {
     const coordinates = await this.fetchLocationData(city);
     return this.fetchWeatherData(coordinates, city);
   }
