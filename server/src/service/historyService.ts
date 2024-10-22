1990a6dbf3d1712b413580a76e8cad61694665d1
VanceGoblin <zvkubajak@gmail.com>
2024-10-22 11:54:00 -0500

    Formatted Code

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -7,10 +7,7 @@ class City {
   id: string;
   name: string;
 
-  constructor(
-    id: string,
-    name: string,
-  ) {
+  constructor(id: string, name: string) {
     this.id = id;
     this.name = name;
   }
@@ -34,6 +31,7 @@ class HistoryService {
       return [];
     }
   }
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   private async write(cities: City[]): Promise<void> {
     try {
@@ -43,16 +41,19 @@ class HistoryService {
       console.error("Error writing to the file:", error);
     }
   }
+
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   async getCities(): Promise<City[]> {
     return await this.read();
   }
+
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: City): Promise<void> {
     const cities = await this.getCities();
     cities.push(city);
     await this.write(cities);
   }
+
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
