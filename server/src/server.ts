776ff31c132bdf8e519db05c37b6a6f2e1ed1c4b
VanceGoblin <zvkubajak@gmail.com>
2024-10-22 11:33:48 -0500

    Created read Method & Set Up filePath

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,19 +9,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// * TODO: Serve static files of entire client dist folder
 
 import path from 'path';
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // app.use(express.static('client/dist'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// * TODO: Implement middleware for parsing JSON and urlencoded form data
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
+// * TODO: Implement middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,3 +1,6 @@
+import fs from "fs/promises";
+import path from "path";
+
 // TODO: Define a City class with name and id properties
 
 class City {
@@ -15,8 +18,22 @@ class City {
 
 // TODO: Complete the HistoryService class
 class HistoryService {
+  private filePath: string;
+
+  constructor() {
+    this.filePath = path.join(__dirname, "searchHistory.json");
+  }
+  
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read(): Promise<City[]> {
+    try {
+      const data = await fs.readFile(this.filePath, "utf-8");
+      return JSON.parse(data);
+    } catch (error) {
+      console.error("Error reading the file:", error);
+      return [];
+    }
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
